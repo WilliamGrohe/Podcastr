2742a9c4cc90699bd7b37f8089e9788c71f304ea
William Grohe <william.grohe@gmail.com>
2021-04-27 00:48:04 -0300

    configurado axios para uso de rotas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Header } from "../components/Header";
+import { GetStaticProps } from "next";
+import { type } from "node:os";
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
       <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
-
 }
 
-export async function getStaticProps() {
-    const response = await fetch('http://localhost:3333/episodes')
-    const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await api.get('episodes', {
+      params: {
+        _limit: 12,
+        _sort: 'published_at',
+        _order: 'desc'
+      }
+    })
+    
 
     return { 
       props: {
