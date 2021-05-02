a5189d359752abd960e62a6c68a9d6e8aad525c5
William Grohe <william.grohe@gmail.com>
2021-05-02 14:33:33 -0300

    Adicionando arquivos de configuração

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse }  from 'axios';
 
 export const api = axios.create({
     baseURL: process.env.API_URL || 'http://localhost:3333'
