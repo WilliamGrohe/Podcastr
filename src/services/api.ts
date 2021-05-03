7ede490e6b52ff3503d8b483573ea1f97d18fff6
William Grohe <william.grohe@gmail.com>
2021-05-02 22:55:30 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse }  from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL:  'http://localhost:3333'
 })
